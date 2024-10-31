e3f2f41bd330458d5b5bf219a231dbe16afcb09b
Gabyplascencia <184980968+Gabyplascencia@users.noreply.github.com>
2024-10-31 11:43:39 -0600

    https://github.com/Gabyplascencia/Project6Tripleten.git

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header('Analisis sobre las preferencias vacaiones en playa o montaña en personas de USA')
+
+st.write('En esta página analizaremos el comportamiento de más de 500,000 residentes americanos y sus preferencias para lugares para descansar, sgun diferentes criterios.')
